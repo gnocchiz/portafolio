5356fe7db1da0beca9e2027072f9424c02dd7950
gnocchiz <lukaribarola@gmail.com>
2024-05-16 11:19:40 +0200

    droga

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 import App from './App';
+import './styles/index.css';
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root')
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
